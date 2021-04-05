ce92657fc1e5cdbf53b8dfeb8a85bc33f9773823
Kuzma Yakimets <k00535872@china.huawei.com>
2021-04-05 17:03:41 +0300

    Added test for is_slicing_criterion

--- test/decomposition/test_slicing.py
+++ test/decomposition/test_slicing.py
@@ -8,6 +8,12 @@ from unittest import TestCase
 
 from program_slicing.decomposition import slicing
 from program_slicing.graph.parse import LANG_JAVA
+from program_slicing.graph.cdg_node import CDGNode, CDG_NODE_TYPE_VARIABLE, CDG_NODE_TYPE_ASSIGNMENT
+
+is_slicing_criterion = slicing.__is_slicing_criterion
+obtain_variable_nodes = slicing.__obtain_variable_nodes
+obtain_seed_statement_nodes = slicing.__obtain_seed_statement_nodes
+obtain_slicing_criteria = slicing.__obtain_slicing_criteria
 
 
 class SlicingTestCase(TestCase):
@@ -29,3 +35,17 @@ class SlicingTestCase(TestCase):
                       "}"
         res = [decomposition for decomposition in slicing.decompose_code(source_code, LANG_JAVA)]
         self.assertEqual(2, len(res))
+
+    def test_is_slicing_criterion(self):
+        a = CDGNode("", CDG_NODE_TYPE_ASSIGNMENT, (1, 1), name="a")
+        b = CDGNode("", CDG_NODE_TYPE_VARIABLE, (2, 2), name="b")
+        c = CDGNode("", CDG_NODE_TYPE_VARIABLE, (3, 3), name="a")
+        self.assertFalse(is_slicing_criterion(a, a))
+        self.assertFalse(is_slicing_criterion(a, b))
+        self.assertTrue(is_slicing_criterion(a, c))
+        self.assertFalse(is_slicing_criterion(b, a))
+        self.assertFalse(is_slicing_criterion(b, b))
+        self.assertFalse(is_slicing_criterion(b, c))
+        self.assertFalse(is_slicing_criterion(c, a))
+        self.assertFalse(is_slicing_criterion(c, b))
+        self.assertFalse(is_slicing_criterion(c, c))
