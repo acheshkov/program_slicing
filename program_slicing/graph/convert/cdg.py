4c521b74c3bfd41352a0cfa78632b61507aa5cbb
Kuzma Yakimets <k00535872@china.huawei.com>
2021-04-09 14:22:44 +0300

    Upgraded documentation for converter.

--- program_slicing/graph/convert/cdg.py
+++ program_slicing/graph/convert/cdg.py
@@ -15,7 +15,8 @@ from program_slicing.graph.cfg_node import CFGNode
 def to_cfg(cdg: ControlDependenceGraph) -> ControlFlowGraph:
     """
     Convert the Control Dependence Graph into a Control Flow Graph.
-    Any changes in the original graph after converting will affect the converted one.
+    New graph will contain links on nodes of the original one so that
+    any changes made after converting in the original graph will affect the converted one.
     :param cdg: Control Dependence Graph that should to be converted.
     :return: Control Flow Graph which nodes contain nodes of the Control Dependence Graph on which it was based on.
     """
