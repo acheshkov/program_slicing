a3b977fcb147426d74d2d5018916e964b6f619ba
Evgeny Maslov <lyriccoder@gmail.com>
2021-05-26 17:12:04 +0300

    Remove comments

--- program_slicing/generate.py
+++ program_slicing/generate.py
@@ -1,23 +1,16 @@
-import operator
-from collections import defaultdict
-from itertools import combinations, combinations_with_replacement
+from itertools import combinations_with_replacement
 
 import tree_sitter
 
-from program_slicing.decomposition.slicing import __obtain_slicing_criteria, __obtain_common_boundary_blocks
-from program_slicing.graph.cfg import ControlFlowGraph
-from program_slicing.graph.manager import ProgramGraphsManager
-from program_slicing.graph.parse import control_flow_graph, LANG_JAVA, program_dependence_graph
-from operator import itemgetter, attrgetter, methodcaller
-from program_slicing.graph.parse import tree_sitter_parsers
 from program_slicing.graph.parse import cdg_java
+from program_slicing.graph.parse import tree_sitter_parsers
 
 parse_node_name = cdg_java.__parse_statement_name
 
 
 def traverse_ast(root, level=0):
     for child in root.children:
-        for ast_and_level in traverse_ast(child, level+1):
+        for ast_and_level in traverse_ast(child, level + 1):
             yield ast_and_level
     yield root, level
 
@@ -45,16 +38,7 @@ def generate_possible_opportunities(filename, function_name, start_line):
     with open(filename) as f:
         source_code = f.read()
 
-    # manager = ProgramGraphsManager(source_code, LANG_JAVA)
-    # cdg = manager.cdg
-    # function_statements = cdg.get_entry_points()
-    # edges = [x for x in cdg.edges]
-    # for function_statement in function_statements:
-    #     slicing_criteria = __obtain_slicing_criteria(cdg, function_statement)
-    #     for variable_statement, seed_statements in slicing_criteria.items():
-    #         common_boundary_blocks = __obtain_common_boundary_blocks(manager, seed_statements)
-    #         print(str((variable_statement, common_boundary_blocks)))
-    counter = 0;
+    counter = 0
     statements_by_block_id = {}
     source_code_bytes = bytes(source_code, "utf8")
     ast = tree_sitter_parsers.java().parse(source_code_bytes).root_node
@@ -74,7 +58,7 @@ def generate_possible_opportunities(filename, function_name, start_line):
     statements_combinations_by_block_id = {}
     for block_id, statements in statements_by_block_id.items():
         id_combinations = [c for c in combinations_with_replacement([idx for idx in range(len(statements))], 2)]
-        statements_combinations = [statements[ids[0]: ids[1]+1] for ids in id_combinations]
+        statements_combinations = [statements[ids[0]: ids[1] + 1] for ids in id_combinations]
         statements_combinations_by_block_id[block_id] = statements_combinations
 
     ranges_by_block_id = {}
@@ -94,50 +78,6 @@ def generate_possible_opportunities(filename, function_name, start_line):
         ranges_by_block_id[block_id] = ranges
 
     print(ranges_by_block_id)
-    # for node in traverse_ast(ast):
-    #     body = node.child_by_field_name("body")
-    #     if body is not None:
-    #         ranges.append((body.start_point, body.end_point))
-
-    # cfg: ControlFlowGraph = control_flow_graph(source_code, LANG_JAVA)
-    # blocks = [(x, x.get_root().start_point[0]) for x in cfg]
-    # sorted_blocks = sorted(blocks,  key=lambda x: x[1])
-    # ignored_blocks = {'catch_formal_parameter', 'parenthesized_expression', 'enhanced_for_variable_declarator'}
-    # blocks = []
-    # for x, start_line in sorted_blocks:
-    #     root = x.get_root()
-    #     # if root.ast_node_type in ignored_blocks:
-    #     #     continue
-    #
-    #     # if root.ast_node_type == 'local_variable_declaration':
-    #     #     inner_stats = root.get_statements()
-    #     #     if inner_stats[-1].ast_node_type == 'for_statement':
-    #     #         continue
-    #     print(f'block {root.start_point[0] + 1}, {root.end_point[0] + 1}, {root.ast_node_type} ')
-    #     t_block = set()
-    #     for j in x.get_statements():
-    #         print(f'\tstat {j.start_point[0] + 1} {j.end_point[0] + 1}, {j.ast_node_type} ')
-    #         t_block.add(j.end_point[0] + 1)
-    #     blocks.append(range(root.start_point[0] + 1, root.end_point[0] + 1))
-    # print(blocks)
-    # for p in cfg.get_entry_points():
-    #     # print(p.get_root().name, p.get_root().ast_node_type)
-    #     stats = p.get_statements()
-    #     for s in stats:
-    #         if s.ast_node_type == 'block':
-    #             print('################')
-    #             for x in range(s.start_point[0], s.end_point[0]):
-    #                 print(x + 1, s.statement_type)
-    #             print('################')
-
-    # pdg = program_dependence_graph(source_code, LANG_JAVA)
-    # for p in pdg.get_entry_points():
-    #     print(p.name, p.end_point[0])
-    #     if p.ast_node_type == 'block':
-    #         print('################')
-    #         for x in range(p.start_point[0], p.end_point[0]):
-    #             print(x + 1)
-    #         print('################')
 
 
 if __name__ == '__main__':
