db1b9a846ca42c4a192d24f136d13cc614c45bd3
Kuzma Yakimets <k00535872@china.huawei.com>
2021-07-15 17:55:26 +0300

    added state for each file while integration test.

--- integration_tests/integration_block_slicing.py
+++ integration_tests/integration_block_slicing.py
@@ -7,7 +7,7 @@ from program_slicing.graph.parse import LANG_JAVA
 
 def run_check(expected_emos, observable_emos):
     for filename, ex_emos in expected_emos.items():
-        print(filename)
+        print(filename, end="\t")
         observable_emos_set = set(observable_emos.get(filename))
         expect_emos_set = set([tuple(x) for x in ex_emos])
         found_obj = observable_emos_set.difference(expect_emos_set)
@@ -16,11 +16,11 @@ def run_check(expected_emos, observable_emos):
         found_obj = expect_emos_set.difference(observable_emos_set)
         if found_obj:
             raise Exception(f'Objects which were not found: {json.dumps(tuple(found_obj))}')
+        print("OK")
 
 
 def main():
     cur_dir = Path(__file__).parent
-
     with open(Path(cur_dir, 'files', 'expected_EMOs.json')) as json_f:
         expected_emos = json.loads(json_f.read())
         observable_emos = {}
@@ -33,7 +33,6 @@ def main():
                         code, LANG_JAVA, min_amount_of_lines=5, max_percentage_of_lines=0.8)
                 }
             observable_emos[java_file.name] = tuple(found_opportunities)
-
     run_check(expected_emos, observable_emos)
 
 
