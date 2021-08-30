4c727cfdc64fc2651e668dd107531f769e126d46
Katya Garmash <katya@LAPTOP-6LP5LUHF.china.huawei.com>
2021-08-30 13:56:24 +0300

    made code examples more compact

--- test/decomposition/test_expand_slice.py
+++ test/decomposition/test_expand_slice.py
@@ -5,33 +5,30 @@ import unittest
 
 class ExpandSliceTestCase(unittest.TestCase):
 
-    code_ex_before = '''public Operand receiveArgs(final ArgsNode argsNode, IR_Scope s) {
-                            final int required = argsNode.getRequiredArgsCount();
-                            final int opt = argsNode.getOptionalArgsCount();
-                            final int rest = argsNode.getRestArg();
+    code_ex_before = '''public Operand receiveA(final AClass a, Scope s) {
+                            final int required = a.getRequired();
+                            final int opt = a.getOpt();
+                            final int rest = a.getRest();
+                                
+                            int index = 1;
                             
-                            int argIndex = 1;
-                            
-                            ListNode preArgs  = argsNode.getPre();
-                            for (int i = 0; i < required; i++, argIndex++) {
-                                ArgumentNode a = (ArgumentNode)preArgs.get(i);
-                                if (a instanceof TypedArgumentNode) {
-                                    TypedArgumentNode t = (TypedArgumentNode)a;
-                                    s.addInstr(new DECLARE_LOCAL_TYPE_Instr(argIndex, buildType(t.getTypeNode())));
+                            LClass pre = a.getPre();
+                            for (int i = 0; i < required; i++, index++) {
+                                if (condition()) {
+                                    do1(pre, index);
                                 }
                             }
                     
                             if (opt > 0 || rest > -1) {
-                                ListNode optArgs = argsNode.getOptArgs();
-                                for (int j = 0; j < opt; j++, argIndex++) {
-                                    Label l = s.getNewLabel();
-                                    LocalAsgnNode n = (LocalAsgnNode)optArgs.get(j);
-                                    s.addInstr(new RECV_OPT_ARG_Instr(new LocalVariable(n.getName()), argIndex, l));
+                                LClass optA = a.getOpt();
+                                for (int j = 0; j < opt; j++, index++) {
+                                    s.addInstr(new Instr(optA), index));
+                                    System.out.println(optA);
                                 }
                     
                                 if (rest > -1) {
-                                    s.addInstr(new ReceiveInst(new LocVar(argsNode.getAN().getName()), argIndex, true));
-                                    argIndex++;
+                                    s.addInstr(new Instr(new LocVar(a.getAN()), index));
+                                    index++;
                                 }
                             }
                         }'''
@@ -41,7 +38,7 @@ class ExpandSliceTestCase(unittest.TestCase):
         """
         output the first 3 minimal expansions from the priority queue
         """
-        slice_to_expand = (18, 30)
+        slice_to_expand = (15, 26)
         expected_extensions = {[3], [4], [3, 4]}
         extension_generator = expand_slices_ordered(self.code_ex_before, slice_to_expand)
         found_extensions = {next(extension_generator) for _ in range(3)}
@@ -52,8 +49,8 @@ class ExpandSliceTestCase(unittest.TestCase):
         """
         output the minimal expansion
         """
-        slice_to_expand = (19, 23)
-        expected_extension = [18]
+        slice_to_expand = (17, 20)
+        expected_extension = [16]
         extension_generator = expand_slices_ordered(self.code_ex_before, slice_to_expand)
         found_extension = next(extension_generator)
         self.assertListEqual(expected_extension, found_extension)
@@ -63,11 +60,11 @@ class ExpandSliceTestCase(unittest.TestCase):
         """
         slice not finished in the end
         """
-        slice_to_expand = (17, 21)
-        expected_extensions = {[18], [22], [18, 22]}
+        slice_to_expand = (17, 18)
+        expected_extension = [19]
         extension_generator = expand_slices_ordered(self.code_ex_before, slice_to_expand)
-        found_extensions = {next(extension_generator) for _ in range(3)}
-        self.assertSetEqual(expected_extensions, found_extensions)
+        found_extension = next(extension_generator)
+        self.assertListEqual(expected_extension, found_extension)
 
     @unittest.skip("not implemented")
     def test_expand_invalid_slice_2(self):
@@ -84,7 +81,7 @@ class ExpandSliceTestCase(unittest.TestCase):
                             }
                             System.out.println(a);
                         }
-                        }'''
+                    }'''
         slice_to_expand = (7, 9)
         expected_extensions = {[4, 5, 6], [3, 4, 5, 6], [2, 3, 4, 5, 6]}
         extension_generator = expand_slices_ordered(code_ex, slice_to_expand)
@@ -97,32 +94,27 @@ class ExpandSliceTestCase(unittest.TestCase):
         output first 3 minimal expansions from the priority queue
         (they reduce the number of IN)
         """
-        code_ex_after = '''public void entry(BasicEntry entry, boolean reusable, long validDuration, TimeUnit timeUnit){        
-                                RouteSpecificPool rospl = getRoutePool(route, true);
-                                if (reusable) {
-                                    if (log.isDebugEnabled()) {
+        code_ex_after = '''public void entry(EClass entry, boolean r, long duration){
+                                RClass rospl = getRospl();
+                                if (r) {
+                                    if (cond()) {
                                         String s;
-                                        if (validDuration > 0) {
-                                            s = "for " + validDuration + " " + timeUnit;
+                                        if (cond2()) {
+                                            s = "for " + duration;
                                         } else {
                                             s = "indefinitely";
                                         }
-                                        log.debug("Pooling connection" +
-                                                " [" + route + "][" + entry.getState() + "]; keep alive " + s);
                                     }
-                                    rospl.freeEntry(entry);
-                                    entry.updateExpiry(validDuration, timeUnit);
-                                    freeConnections.add(entry);
+                                    entry.update(duration);
                                 } else {
                                     rospl.dropEntry();
-                                    numConnections--;
                                 }
-                                System.out.println('Something');
+                                System.out.println('Message');
                                 notifyWaitingThread(rospl);
-                                someCommand(reusable);
+                                someCommand(r);
                             }'''
-        slice_to_expand = (2, 20)
-        expected_extensions = {[22], [23], [22, 23]}
+        slice_to_expand = (2, 15)
+        expected_extensions = {[17], [18], [17, 18]}
         extension_generator = expand_slices_ordered(code_ex_after, slice_to_expand)
         found_extensions = {next(extension_generator) for _ in range(3)}
         self.assertSetEqual(expected_extensions, found_extensions)
