376bcefc770e4be3b1c6297bd6f1d41326753570
Evgeny Maslov <lyriccoder@gmail.com>
2021-09-09 12:00:36 +0300

    add requirements

--- benchmarks/block_slices/check_perfomance.py
+++ benchmarks/block_slices/check_perfomance.py
@@ -6,12 +6,28 @@ import traceback
 import sys
 
 import tqdm
+from cchardet import detect
 from numpy import mean, median, quantile
-from veniq.utils.encoding_detector import read_text_with_autodetected_encoding
 
 from program_slicing.decomposition.block_slicing import get_block_slices
 from program_slicing.decomposition.slice_predicate import SlicePredicate
 from program_slicing.graph.parse import LANG_JAVA
+
+
+def detect_encoding_of_data(data: bytes):
+    return detect(data)['encoding']
+
+
+def read_text_with_autodetected_encoding(filename: str):
+    with open(filename, 'rb') as target_file:
+        data = target_file.read()
+
+    if not data:
+        return ''  # In case of empty file, return empty string
+
+    encoding = detect_encoding_of_data(data) or 'utf-8'
+    return data.decode(encoding)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run benchmark for block slicing')
@@ -26,11 +42,6 @@ if __name__ == '__main__':
         type=int,
         default=1,
         help="Number of iterations to run benchmark script for all files. "
-    )
-    parser.add_argument(
-        "-csv", "--dataset_csv",
-        help='Csv file location with benchmark data',
-        type=Path
     )
 
     args = parser.parse_args()
@@ -48,9 +59,9 @@ if __name__ == '__main__':
     for _ in tqdm.tqdm(range(args.iterations)):
         for java_file in tqdm.tqdm(java_files):
             text = read_text_with_autodetected_encoding(str(java_file))
-            start_time = time()
-            start_datetime = datetime.datetime.now()
             try:
+                start_time = time()
+                start_datetime = datetime.datetime.now()
                 slices = get_block_slices(
                     text,
                     LANG_JAVA,
@@ -67,7 +78,6 @@ if __name__ == '__main__':
             except:
                 print(f'Error while reading {java_file}')
                 exc_type, exc_value, exc_traceback = sys.exc_info()
-                #traceback.print_tb(exc_traceback, file=sys.stdout)
                 traceback.print_exception(exc_type, exc_value, exc_traceback, file=sys.stdout)
 
     avg_sec = mean(arr_with_time_in_seconds)
@@ -81,4 +91,3 @@ if __name__ == '__main__':
           f'quantile 95% {quantile(arr_with_time_in_seconds, 0.95):0.10f} secs')
 
     previous_best_time = 0.17  # in secs for laptop
-    assert avg_sec < 0.18
