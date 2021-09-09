5efdd57e789dd5ccd0358407a35a597f38101bab
Evgeny Maslov <lyriccoder@gmail.com>
2021-09-09 12:17:31 +0300

    Fix files

--- integration_tests/performance/block_slices/dataset/EditorCaret_runTransaction_100.0_31.0_94.0_2.java
+++
@@ -1,3 +0,0 @@
-private int runTransaction(CaretTransaction.RemoveType removeType, int offset, CaretItem[] addCarets, CaretMoveHandler moveHandler) {
-        return runTransaction(removeType, offset, addCarets, moveHandler, MoveCaretsOrigin.DEFAULT);
-    }
