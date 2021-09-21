771d0583f7a38ba58e4d7e9897104ca63978387f
Kuzma Yakimets <k00535872@china.huawei.com>
2021-09-21 10:08:35 +0300

    structure for extending filters.

--- program_slicing/decomposition/slice_predicate.py
+++ program_slicing/decomposition/slice_predicate.py
@@ -47,16 +47,16 @@ class SlicePredicate:
         self.__program_slice = None
         self.__manager = None
 
-    def __call__(self, program_slice: ProgramSlice) -> bool:
+    def __call__(self, program_slice: ProgramSlice, **kwargs) -> bool:
         if program_slice is None:
             raise ValueError("Program slice has to be defined")
         self.__program_slice = program_slice
         for checker in self.__checkers:
-            if not checker():
+            if not checker(**kwargs):
                 return False
         return True
 
-    def __check_min_amount_of_statements(self) -> bool:
+    def __check_min_amount_of_statements(self, **kwargs) -> bool:
         if self.__min_amount_of_statements is None:
             return True
         if self.__manager is None:
@@ -65,7 +65,7 @@ class SlicePredicate:
             return False
         return True
 
-    def __check_max_amount_of_statements(self) -> bool:
+    def __check_max_amount_of_statements(self, **kwargs) -> bool:
         if self.__max_amount_of_statements is None:
             return True
         if self.__manager is None:
@@ -75,21 +75,21 @@ class SlicePredicate:
             return False
         return True
 
-    def __check_min_amount_of_lines(self) -> bool:
+    def __check_min_amount_of_lines(self, **kwargs) -> bool:
         if self.__min_amount_of_lines is None:
             return True
         if len(self.__program_slice.lines) < self.__min_amount_of_lines:
             return False
         return True
 
-    def __check_max_amount_of_lines(self) -> bool:
+    def __check_max_amount_of_lines(self, **kwargs) -> bool:
         if self.__max_amount_of_lines is None:
             return True
         if len(self.__program_slice.lines) > self.__max_amount_of_lines:
             return False
         return True
 
-    def __check_lines_are_full(self) -> bool:
+    def __check_lines_are_full(self, **kwargs) -> bool:
         if self.__lines_are_full is None:
             return True
         source_lines = self.__program_slice.source_lines
@@ -110,7 +110,7 @@ class SlicePredicate:
                     return False
         return True
 
-    def __check_parsing(self) -> bool:
+    def __check_parsing(self, **kwargs) -> bool:
         if self.__lang_to_check_parsing is None:
             return True
         code_bytes = bytes(self.__program_slice.code, "utf-8")
@@ -136,7 +136,7 @@ class SlicePredicate:
                     return False
         return self.__check_no_broken_goto()
 
-    def __check_no_broken_goto(self) -> bool:
+    def __check_no_broken_goto(self, **kwargs) -> bool:
         cdg = self.__manager.get_control_dependence_graph()
         for statement in cdg:
             if statement.statement_type == StatementType.GOTO:
@@ -144,7 +144,7 @@ class SlicePredicate:
                     return False
         return True
 
-    def __check_has_returnable_variable(self) -> bool:
+    def __check_has_returnable_variable(self, **kwargs) -> bool:
         if self.__has_returnable_variable is None:
             return True
         if self.__manager is None:
@@ -160,7 +160,7 @@ class SlicePredicate:
                     return True
         return False
 
-    def __check_forbidden_words(self) -> bool:
+    def __check_forbidden_words(self, **kwargs) -> bool:
         if self.__forbidden_words is None:
             return True
         code = self.__program_slice.code
@@ -176,10 +176,15 @@ def check_slice(
         max_amount_of_statements: int = None,
         min_amount_of_lines: int = None,
         max_amount_of_lines: int = None,
+        min_percentage_of_statements: float = None,
+        max_percentage_of_statements: float = None,
+        min_percentage_of_lines: float = None,
+        max_percentage_of_lines: float = None,
         lines_are_full: bool = None,
         lang_to_check_parsing: str = None,
         has_returnable_variable: bool = None,
-        forbidden_words: Set[str] = None) -> bool:
+        forbidden_words: Set[str] = None,
+        context: ProgramGraphsManager = None) -> bool:
     """
     Check a ProgramSlice if it matches specified conditions.
     :param program_slice: slice that should to be checked.
@@ -189,11 +194,22 @@ def check_slice(
     Will raise Exception if lang_to_check_parsing is not specified.
     :param min_amount_of_lines: minimal acceptable amount of lines.
     :param max_amount_of_lines: maximal acceptable amount of lines.
+    :param min_percentage_of_statements: minimal acceptable share of Statements (float number [0-1]).
+    Will raise Exception if lang_to_check_parsing is not specified.
+    Will raise Exception if context is not specified.
+    :param max_percentage_of_statements: maximal acceptable share of Statements (float number [0-1]).
+    Will raise Exception if lang_to_check_parsing is not specified.
+    Will raise Exception if context is not specified while calling.
+    :param min_percentage_of_lines: minimal acceptable share of lines (float number [0-1]).
+    Will raise Exception if context is not specified while calling.
+    :param max_percentage_of_lines: maximal acceptable share of lines (float number [0-1]).
+    Will raise Exception if context is not specified while calling.
     :param lines_are_full: check if the slice contains only entire lines.
     :param lang_to_check_parsing: language in which slice should to be compilable.
     :param has_returnable_variable: slice should to have a declaration of variable that may be returned if needed.
     Will raise Exception if lang_to_check_parsing is not specified.
     :param forbidden_words: a set of substrings that shouldn't be found in a slice code.
+    :param context: a ProgramGraphsManager that defines context of the given ProgramSlice.
     :return: True if slice matches specified conditions.
     """
     return SlicePredicate(
@@ -205,4 +221,4 @@ def check_slice(
         lang_to_check_parsing=lang_to_check_parsing,
         has_returnable_variable=has_returnable_variable,
         forbidden_words=forbidden_words
-    )(program_slice)
+    )(program_slice, context=context)
