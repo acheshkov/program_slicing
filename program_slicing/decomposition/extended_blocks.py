65a0af888929e7ceacd78f62f47d49889115ca38
Katya Garmash <katya@LAPTOP-6LP5LUHF.china.huawei.com>
2021-09-28 11:53:33 +0300

    wip

---
+++ program_slicing/decomposition/extended_blocks.py
@@ -0,0 +1,269 @@
+__licence__ = 'MIT'
+__author__ = 'KatGarmash'
+__credits__ = ['KatGarmash']
+__maintainer__ = 'KatGarmash'
+__date__ = '2021/09/14'
+
+from functools import reduce
+from itertools import chain, combinations_with_replacement, filterfalse, combinations
+from typing import Set, Tuple, Dict, Iterable, List
+from heapq import heappush, heappop
+
+from program_slicing.decomposition.block_slicing import __percentage_or_amount_exceeded
+from program_slicing.decomposition.program_slice import ProgramSlice
+from program_slicing.decomposition.slice_predicate import SlicePredicate
+from program_slicing.graph.cdg import ControlDependenceGraph
+from program_slicing.graph.ddg import DataDependenceGraph
+from program_slicing.graph.manager import ProgramGraphsManager
+from program_slicing.graph.pdg import ProgramDependenceGraph
+from program_slicing.graph.statement import Statement, StatementType
+
+Block = Set[Statement]
+VariableDefinition = Statement
+VariableUse = Statement
+
+
+def __temp__get_block_slice_statements(
+        source_code: str,
+        lang: str,
+        slice_predicate: SlicePredicate = None,
+        max_percentage_of_lines: float = None,
+        may_cause_code_duplication: bool = False) -> Iterable[ProgramSlice]:
+    
+    source_lines = source_code.split("\n")
+    manager = ProgramGraphsManager(source_code, lang)
+    for scope in manager.scope_statements:
+        function_statement = manager.get_function_statement(scope)
+        if function_statement is None:
+            continue
+        function_length = function_statement.end_point.line_number - function_statement.start_point.line_number + 1
+        general_statements = sorted((
+            statement
+            for statement in manager.get_statements_in_scope(scope)
+            if statement in manager.general_statements),
+            key=lambda x: (x.start_point, -x.end_point))
+        id_combinations = [
+            c for c in combinations_with_replacement([idx for idx in range(len(general_statements))], 2)
+        ]
+        for ids in id_combinations:
+            current_statements = general_statements[ids[0]: ids[1] + 1]
+            if not current_statements:
+                continue
+            if max_percentage_of_lines is not None and __percentage_or_amount_exceeded(
+                    function_length,
+                    current_statements[-1].end_point.line_number - current_statements[0].start_point.line_number + 1,
+                    max_percentage_of_lines):
+                continue
+            extended_statements = manager.get_statements_in_range(
+                current_statements[0].start_point,
+                current_statements[-1].end_point)
+            if not may_cause_code_duplication:
+                affecting_statements = manager.get_affecting_statements(extended_statements)
+                if len(manager.get_used_variables(affecting_statements)) > 1 or \
+                        manager.contain_redundant_statements(extended_statements):
+                    continue
+            if len(manager.get_exit_statements(extended_statements)) > 1:
+                continue
+            program_slice = ProgramSlice(source_lines).from_statements(extended_statements)
+            if slice_predicate is None or slice_predicate(program_slice, scopes=manager.scope_statements):
+                yield extended_statements
+
+def expand_slices_ordered(top_k=None):
+    pass
+
+
+
+def __get_incoming_variables(block_statements: Block,
+                             manager: ProgramGraphsManager,
+                             require_variable_declaration: bool = False
+                            ) -> Dict[str, VariableUse] :
+    """
+    :param block_statements: block to look for variables in
+    :param manager: manage for graphs of the whole program
+    :return: dictionary from name of varibable in the block to a statement
+            in a block that uses it and is data-dependendent on a statement
+            outside the block
+    """
+    ddg = manager.get_data_dependence_graph()
+    incoming_variables = dict()
+    for statement in block_statements:
+        for data_dom in ddg.predecessors(statement):
+            # can be 1-many map, but it's ok for our purposes
+            if data_dom not in block_statements and data_dom.name not in incoming_variables:
+                if __flow_dep_given_data_dep(statement, data_dom):
+                #if not require_variable_declaration:
+                #    if data_dom.name == statement.name:
+                #        continue
+                    incoming_variables[data_dom.name] = statement
+    return incoming_variables
+
+
+def __get_outgoing_variables(block_statements: Block,
+                             manager: ProgramGraphsManager
+                             ) -> Dict[str, VariableDefinition]:
+    ddg = manager.get_data_dependence_graph()
+    outgoing_variables: Dict[str, VariableDefinition] = dict()
+    for statement in block_statements:
+        for flow_dep in set(ddg.successors(statement)).difference(block_statements):
+            if __flow_dep_given_data_dep(flow_dep, statement):
+                outgoing_variables[statement.name] = statement
+                break
+    return outgoing_variables
+
+
+def __flow_dep_given_data_dep(statement_1, statement_2, variable_name = None) -> bool:
+    """
+    Check if statement_1 is flow-dependent on statement_2
+    """
+    if statement_2.statement_type not in [StatementType.ASSIGNMENT,
+                                          StatementType.VARIABLE,
+                                          StatementType.FUNCTION]:
+        return False
+
+    if statement_1.name != statement_2.name:
+        if variable_name is not None:
+            return statement_2.name == variable_name
+        return True
+
+    return False
+
+
+def __compute_backward_slice_recursive(statement: Statement,
+                                       variable_name: str,
+                                       original_block: Set[Statement],
+                                       cdg: ControlDependenceGraph,
+                                       ddg: DataDependenceGraph,
+                                       backward_slice: Set):
+    backward_slice.add(statement)
+    cdg_predecessors = cdg.predecessors(statement)
+    flow_predecessors = filter(lambda x: __flow_dep_given_data_dep(statement, x, variable_name=variable_name),
+                                                ddg.predecessors(statement))
+    for predecessor in set(chain(cdg_predecessors, flow_predecessors)):
+        if predecessor.statement_type == StatementType.FUNCTION:
+            continue
+        new_variable_name = None
+        if predecessor in original_block:
+            new_variable_name = variable_name
+        __compute_backward_slice_recursive(predecessor, new_variable_name, original_block, cdg, ddg, backward_slice)
+
+
+def __compute_backward_slice(variable_use: VariableUse,
+                             variable_name: str,
+                             original_block: Set[Statement],
+                             manager: ProgramGraphsManager
+                            ) -> Set[Statement]:
+    backward_slice: Set[Statement] = set()
+    cdg = manager.get_control_dependence_graph()
+    ddg = manager.get_program_dependence_graph()
+    __compute_backward_slice_recursive(variable_use, variable_name, original_block, cdg, ddg, backward_slice)
+    return backward_slice
+
+
+def __compute_forward_slice(variable_def: VariableDefinition,
+                            manager: ProgramGraphsManager
+                           ) -> Set[Statement]:
+    ddg = manager.get_data_dependence_graph()
+    forward_slice = set(ddg.successors(variable_def))
+    return forward_slice
+
+
+def __extend_block_singleton(block_statements: Block,
+                             manager: ProgramGraphsManager
+                            ) -> List[Tuple[Block,
+                                            Dict[str, VariableUse],
+                                            Dict[str, VariableDefinition],
+                                            str]]:
+    incoming_variables = __get_incoming_variables(block_statements, manager)
+    outgoing_variables = __get_outgoing_variables(block_statements, manager)
+
+    singleton_extensions: List[Tuple[Block,
+                                     Dict[str, VariableDefinition],
+                                     Dict[str, VariableUse],
+                                     str]] = list()
+    singleton_extensions.append((block_statements, incoming_variables, outgoing_variables, ''))
+
+    for var_name in chain(incoming_variables.keys(), outgoing_variables.keys()):
+        new_block = block_statements.copy()
+        new_incoming_variables = incoming_variables.copy()
+        new_outgoing_variables = outgoing_variables.copy()
+
+        if var_name in incoming_variables:
+            variable_use = incoming_variables[var_name]
+            predecessor = next(manager.get_data_dependence_graph().predecessors(variable_use))
+            #if predecessor.statement_type != StatementType.FUNCTION:
+            if True:
+                backward_slice = __compute_backward_slice(variable_use, var_name, block_statements, manager)
+                new_block |= backward_slice
+                del new_incoming_variables[var_name]
+
+        if var_name in outgoing_variables:
+            variable_def = outgoing_variables[var_name]
+            forward_slice = __compute_forward_slice(variable_def, manager)
+            new_block |= forward_slice
+            del new_outgoing_variables[var_name]
+
+        singleton_extensions.append((new_block, new_incoming_variables, new_outgoing_variables, var_name))
+
+    return singleton_extensions
+
+
+def __filter_anti_dependence(new_statements, original_slice, manager):
+    ddg = manager.get_data_dependence_graph()
+    for statement in new_statements:
+        if statement.end_point > original_slice.ranges[0]:
+            continue
+        for data_successor in ddg.successors(statement):
+            if data_successor.end_point > original_slice.ranges[0]:
+                continue
+            if __flow_dep_given_data_dep(data_successor, statement):
+                return False
+
+    return True
+
+
+def __filter_control_dependence(new_statements, original_slice, manager):
+    # TODO
+    pass
+
+
+def __filter_valid(slice_candidate: Set[Statement],
+                 original_slice: ProgramSlice,
+                 manager: ProgramGraphsManager) -> bool:
+    new_statements = slice_candidate.difference(original_slice)
+
+    # anti-dependence
+    if not __filter_anti_dependence(new_statements, original_slice, manager):
+        return False
+
+    # more than 1 outgoing vars
+    outgoing_variables = __get_outgoing_variables(slice_candidate, manager)
+    if len(outgoing_variables.keys()) > 1:
+        return False
+
+    # control dep
+    if not __filter_control_dependence(new_statements, original_slice, manager):
+        return False
+
+    return True
+
+
+def __compute_cost(full_extension, param):
+    pass
+
+
+def get_block_extensions_ordered(block_statements: Block,
+                                 manager: ProgramGraphsManager
+                                ) -> Iterable[ProgramSlice, float]:
+
+    singleton_extensions = __extend_block_singleton(block_statements, manager)
+
+    pq = []
+    for variable_id_subset in chain.from_iterable(combinations(singleton_extensions, r)
+                                                  for r in range(1, len(singleton_extensions) + 1)):
+        full_extension = reduce(lambda x, y: x.union(singleton_extensions[y][0]), variable_id_subset, set())
+        if __filter_valid(full_extension, block_statements, manager):
+            cost = __compute_cost(full_extension, [singleton_extensions[i] for i in variable_id_subset])
+            heappush(pq, (cost, full_extension))
+
+    while pq:
+        yield heappop(pq)
--- test/decomposition/test_expand_slice.py
+++ test/decomposition/test_expand_slice.py
@@ -1,7 +1,24 @@
-import unittest
+__licence__ = 'MIT'
+__author__ = 'KatGarmash'
+__credits__ = ['KatGarmash']
+__maintainer__ = 'KatGarmash'
+__date__ = '2021/09/14'
 
-# from program_slicing.decomposition.expand_slice import expand_slices_ordered,\
-#                                                        InvalidBlockException
+
+import unittest
+from typing import Tuple, Dict, List, Set
+
+from program_slicing.decomposition import extended_blocks
+from program_slicing.decomposition.extended_blocks import expand_slices_ordered, Block
+from program_slicing.decomposition.program_slice import ProgramSlice
+from program_slicing.graph.manager import ProgramGraphsManager
+from program_slicing.graph.parse import LANG_JAVA
+from program_slicing.graph.point import Point
+from program_slicing.graph.statement import Statement
+
+get_incoming_variables = extended_blocks.__get_incoming_variables
+get_outgoing_variables = extended_blocks.__get_outgoing_variables
+extend_block_singleton = extended_blocks.__extend_block_singleton
 
 
 class ExpandSliceTestCase(unittest.TestCase):
@@ -60,7 +77,7 @@ class ExpandSliceTestCase(unittest.TestCase):
                     }
                 }'''
         slice_to_expand = (2, 3)
-        self.assertRaises(InvalidBlockException, expand_slices_ordered(code_ex, slice_to_expand))
+        self.assertRaises(Exception, expand_slices_ordered(code_ex, slice_to_expand))
 
     @unittest.skip("not implemented")
     def test_expand_invalid_slice_2(self):
@@ -123,3 +140,219 @@ class ExpandSliceTestCase(unittest.TestCase):
         extension_generator = expand_slices_ordered(code_ex_multiple_vars, slice_to_expand)
         found_extensions = {next(extension_generator) for _ in range(3)}
         self.assertSetEqual(expected_extensions, found_extensions)
+
+    def test_get_incoming_variables_1(self):
+        code_1 = '''public void methodEx(final AClass a) {
+                            final int opt = a.getOpt();
+                            final int rest = a.getRest();
+                            int i = 1;
+                            if (opt > 0 || rest > -1) {
+                                do2(i);
+                            }
+        }'''
+        manager = ProgramGraphsManager(code_1, LANG_JAVA)
+        block_11 = manager.get_statements_in_range(Point(4, 0), Point(6, 10000))
+        incoming_variables_11 = get_incoming_variables(block_11, manager)
+        self.assertSetEqual(set(incoming_variables_11.keys()), {'opt', 'rest', 'i'})
+
+    def test_get_incoming_variables_2(self):
+        code = '''public void methodEx() {
+                                int i = 1;
+                                do(i);
+                                i = 2;
+                                do2(i);
+            }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(3, 0), Point(4, 10000))
+        incoming_variables = get_incoming_variables(block, manager)
+        self.assertSetEqual(set(incoming_variables.keys()), set())
+
+    def test_get_incoming_variables_3(self):
+        code = '''public void methodEx(final AClass a) {
+                    final int opt = a.getOpt();
+                    int i = 1;
+                    do1(i);
+                    if (opt > 0) {
+                        LClass optA = a.getOpt();
+                        for (int j = 0; j < opt; j++) {
+                            System.out.println(optA);
+                        }
+                    }
+                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(6, 0), Point(8, 10000))
+        incoming_variables = get_incoming_variables(block, manager)
+        self.assertSetEqual(set(incoming_variables.keys()), {'opt', 'optA'})
+
+    def test_outgoing_variables_1(self):
+        code = '''public void methodEx(boolean a){
+                                    RClass r = getR();
+                                    if (a) {
+                                        r.update();
+                                    }
+                                    System.out.println('Message');
+                                    do1(r);
+                                    do2(a);
+                                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(1, 0), Point(4, 10000))
+        outgoing_variables = get_outgoing_variables(block, manager)
+        self.assertSetEqual(set(outgoing_variables.keys()), {'r'})
+
+    def test_outgoing_variables_2(self):
+        code = '''public void methodEx(boolean a){
+                                    RClass r = getR();
+                                    if (a) {
+                                        r.update();
+                                    }
+                                    System.out.println('Message');
+                                    r = updateR();
+                                    do1(r);
+                                    do2(a);
+                                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(1, 0), Point(4, 10000))
+        outgoing_variables = get_outgoing_variables(block, manager)
+        self.assertSetEqual(set(outgoing_variables.keys()), set())
+
+    def __compare_extended_slices(self, **kwargs):
+        result_extension = kwargs["extension"]
+        expected_range = kwargs["expected_range"]
+        expected_in = kwargs["expected_in"]
+        expected_out = kwargs["expected_out"]
+
+        _range = [(r[0].line_number, r[1].line_number)
+                  for r in ProgramSlice(kwargs['source_code']).from_statements(result_extension[0]).ranges_compact()]
+        in_vars = set(result_extension[1].keys())
+        out_vars = set(result_extension[2].keys())
+        self.assertListEqual(expected_range, _range)
+        self.assertSetEqual(expected_in, in_vars)
+        self.assertSetEqual(expected_out, out_vars)
+
+    def test_extend_block_singleton_1(self):
+        code = '''public void method() {
+                    int a = 1;
+                    int b = 2;
+                    inv1(a, b);
+                    inv2(a);
+                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(4, 0), Point(4, 10000))
+        singleton_extensions = extend_block_singleton(block, manager)
+        name_to_extension = {}
+        for ext in singleton_extensions:
+            name_to_extension[ext[3]] = ext
+        self.assertSetEqual({'', 'a'}, set(name_to_extension.keys()))
+
+        self.__compare_extended_slices(extension=name_to_extension['a'],
+                                       expected_range=[(1, 1), (4, 4)],
+                                       expected_in=set(),
+                                       expected_out=set(),
+                                       source_code=code)
+
+        self.__compare_extended_slices(extension=name_to_extension[''],
+                                       expected_range=[(4, 4)],
+                                       expected_in={'a'},
+                                       expected_out=set(),
+                                       source_code=code)
+
+    def test_extend_block_singleton_2(self):
+        code = '''public void methodEx(final AClass a) {
+                            final int opt = a.getOpt();
+                            final int rest = a.getRest();
+                            int i = 1;
+                            do1(i);
+                            if (opt > 0 || rest > -1) {
+                                do2(i);
+                            }
+                        }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(5, 0), Point(7, 10000))
+        singleton_extensions = extend_block_singleton(block, manager)
+        name_to_extension = {}
+        for ext in singleton_extensions:
+            name_to_extension[ext[3]] = ext
+        self.assertSetEqual({'', 'opt', 'rest', 'i'}, set(name_to_extension.keys()))
+
+        self.__compare_extended_slices(extension = name_to_extension[''],
+                                       expected_range = [(5, 5), (6, 6), (7, 7)],
+                                       expected_in = {'opt', 'rest', 'i'},
+                                       expected_out = set(),
+                                       source_code = code
+                                       )
+
+        self.__compare_extended_slices(extension=name_to_extension['opt'],
+                                       expected_range=[(1, 1), (5, 5), (6, 6), (7, 7)],
+                                       expected_in={'rest', 'i'},
+                                       expected_out=set(),
+                                       source_code = code
+                                       )
+
+        self.__compare_extended_slices(extension=name_to_extension['rest'],
+                                       expected_range=[(2, 2), (5, 5), (6, 6), (7, 7)],
+                                       expected_in={'opt', 'i'},
+                                       expected_out=set(),
+                                       source_code=code
+                                       )
+
+        self.__compare_extended_slices(extension=name_to_extension['i'],
+                                       expected_range=[(3, 3), (5, 5), (6, 6), (7, 7)],
+                                       expected_in={'opt', 'rest'},
+                                       expected_out=set(),
+                                       source_code=code
+                                       )
+
+    def test_extend_block_singleton_3(self):
+        code = '''public void methodEx(){
+                    RClass r = getR();
+                    if (cond()) {
+                        r.update();
+                    }
+                    System.out.println('Message');
+                    do1(r);
+                    do2(a);
+                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(1, 0), Point(4, 10000))
+        singleton_extensions = extend_block_singleton(block, manager)
+        name_to_extension = {}
+        for ext in singleton_extensions:
+            name_to_extension[ext[3]] = ext
+        self.assertSetEqual({'', 'r'}, set(name_to_extension.keys()))
+
+        self.__compare_extended_slices(extension=name_to_extension[''],
+                                       expected_range=[(1, 1), (2, 2), (3, 3), (4, 4)],
+                                       expected_in=set(),
+                                       expected_out={'r'},
+                                       source_code=code
+                                       )
+
+        self.__compare_extended_slices(extension=name_to_extension['r'],
+                                       expected_range=[(1, 1), (2, 2), (3, 3), (4, 4), (6, 6)],
+                                       expected_in=set(),
+                                       expected_out=set(),
+                                       source_code=code
+                                       )
+
+    @unittest.skip("need to fix bug in parser")
+    def test_extend_block_singleton_4(self):
+        code = '''public void methodEx(boolean a){
+                    System.out.println();
+                    if (cond(a)) {
+                        bla();
+                    }
+                }'''
+        manager = ProgramGraphsManager(code, LANG_JAVA)
+        block = manager.get_statements_in_range(Point(2, 0), Point(3, 10000))
+        singleton_extensions = extend_block_singleton(block, manager)
+        name_to_extension = {}
+        for ext in singleton_extensions:
+            name_to_extension[ext[3]] = ext
+        self.assertSetEqual({''}, set(name_to_extension.keys()))
+
+        self.__compare_extended_slices(extension=name_to_extension[''],
+                                       expected_range=[(1, 1), (2, 2), (3, 3), (4, 4)],
+                                       expected_in={'a'},
+                                       expected_out=set(),
+                                       source_code=code
+                                       )
