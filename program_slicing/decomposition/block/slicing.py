78763cf210ec2490cae93e664b9fba2e628146a9
Kuzma Yakimets <k00535872@china.huawei.com>
2021-09-29 19:16:14 +0300

    added posibility to unite statements into groups in block slicing to increase generating speed.

--- program_slicing/decomposition/block/slicing.py
+++ program_slicing/decomposition/block/slicing.py
@@ -10,7 +10,7 @@ from typing import Iterable, List, Tuple
 from program_slicing.decomposition.program_slice import ProgramSlice
 from program_slicing.decomposition.slice_predicate import SlicePredicate
 from program_slicing.graph.manager import ProgramGraphsManager
-from program_slicing.graph.statement import Statement
+from program_slicing.graph.statement import Statement, StatementType
 
 
 def get_block_slices(
@@ -18,7 +18,8 @@ def get_block_slices(
         lang: str,
         slice_predicate: SlicePredicate = None,
         include_noneffective: bool = True,
-        may_cause_code_duplication: bool = False) -> Iterable[ProgramSlice]:
+        may_cause_code_duplication: bool = False,
+        unite_statements_into_groups: bool = False) -> Iterable[ProgramSlice]:
     """
     For each a specified source code generate list of Program Slices based on continues blocks.
     :param source_code: source code that should be decomposed.
@@ -26,6 +27,7 @@ def get_block_slices(
     :param slice_predicate: SlicePredicate object that describes which slices should be filtered. No filtering if None.
     :param include_noneffective: include comments and blank lines to a slice if True.
     :param may_cause_code_duplication: allow to generate slices which extraction will cause code duplication if True.
+    :param unite_statements_into_groups: will unite function calls, assignment and declarations into groups if True.
     :return: generator of the ProgramSlices.
     """
     source_lines = source_code.split("\n")
@@ -40,17 +42,31 @@ def get_block_slices(
             for statement in statements_in_scope
             if statement in manager.general_statements),
             key=lambda x: (x.start_point, -x.end_point))
+        last_general_group = []
+        general_groups = []
+        for statement in general_statements:
+            if unite_statements_into_groups and statement.statement_type in {
+                StatementType.UNKNOWN, StatementType.ASSIGNMENT, StatementType.VARIABLE, StatementType.CALL
+            }:
+                last_general_group.append(statement)
+            else:
+                if last_general_group:
+                    general_groups.append(last_general_group)
+                general_groups.append([statement])
+                last_general_group = []
+        if last_general_group:
+            general_groups.append(last_general_group)
         id_combinations = (
-            c for c in combinations_with_replacement([idx for idx in range(len(general_statements))], 2)
-            if __pre_check(general_statements, c, slice_predicate, source_lines)
+            c for c in combinations_with_replacement([idx for idx in range(len(general_groups))], 2)
+            if __pre_check(general_groups, c, slice_predicate, source_lines)
         )
         for ids in id_combinations:
-            current_statements = general_statements[ids[0]: ids[1] + 1]
-            if not current_statements:
+            current_groups = general_groups[ids[0]: ids[1] + 1]
+            if not current_groups:
                 continue
             extended_statements = manager.get_statements_in_range(
-                current_statements[0].start_point,
-                current_statements[-1].end_point)
+                current_groups[0][0].start_point,
+                current_groups[-1][-1].end_point)
             if not may_cause_code_duplication:
                 affecting_statements = manager.get_affecting_statements(extended_statements)
                 if len(manager.get_used_variables(affecting_statements)) > 1 or \
@@ -67,28 +83,28 @@ def get_block_slices(
 
 
 def __pre_check(
-        statements: List[Statement],
+        groups: List[List[Statement]],
         ids: Tuple[int, ...],
         slice_predicate: SlicePredicate,
         source_lines: List[str]) -> bool:
     if slice_predicate:
         if slice_predicate.min_amount_of_lines is not None:
             max_lines_number = \
-                statements[ids[1]].end_point.line_number - statements[ids[0]].start_point.line_number + 1
+                groups[ids[1]][-1].end_point.line_number - groups[ids[0]][0].start_point.line_number + 1
             if max_lines_number < slice_predicate.min_amount_of_lines:
                 return False
         if slice_predicate.max_amount_of_statements is not None:
-            min_statements_number = ids[1] - ids[0] + 1
+            min_statements_number = sum(len(groups[i]) for i in range(ids[0], ids[1] + 1))
             if min_statements_number > slice_predicate.max_amount_of_statements:
                 return False
         if slice_predicate.lines_are_full is not None:
-            line_n = statements[ids[0]].start_point.line_number
-            column_n = statements[ids[0]].start_point.column_number
+            line_n = groups[ids[0]][0].start_point.line_number
+            column_n = groups[ids[0]][0].start_point.column_number
             line_part = source_lines[line_n][:column_n]
             if "//" not in line_part and any(c != ' ' and c != '\t' and c != '\n' and c != '\r' for c in line_part):
                 return not slice_predicate.lines_are_full
-            line_n = statements[ids[1]].end_point.line_number
-            column_n = statements[ids[1]].end_point.column_number
+            line_n = groups[ids[1]][-1].end_point.line_number
+            column_n = groups[ids[1]][-1].end_point.column_number
             line_part = source_lines[line_n][column_n:]
             if "//" not in line_part and any(c != ' ' and c != '\t' and c != '\n' and c != '\r' for c in line_part):
                 return not slice_predicate.lines_are_full
